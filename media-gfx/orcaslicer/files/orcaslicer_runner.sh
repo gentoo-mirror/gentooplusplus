43dca66d1bc753d193aaed5c12956baedb17a362
Eugeniusz-Gienek <eugene@sky.community>
2025-03-02 15:14:16 +0100

    adding info message

--- media-gfx/orcaslicer/files/orcaslicer_runner.sh
+++ media-gfx/orcaslicer/files/orcaslicer_runner.sh
@@ -4,4 +4,4 @@ if [ ! -f ~/.config/orcaslicer/configured ]; then
     touch ~/.config/orcaslicer/configured
     /opt/orcaslicer/DockerBuild2.sh
 fi
-/opt/orcaslicer/DockerRun.sh
+/opt/orcaslicer/DockerRun.sh || echo " [ INFO ] If you have just reinstalled the package, please remove the file ~/.config/orcaslicer/configured and restart program."
